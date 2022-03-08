55967803d206246e74ed6a323e913883df7cb4a5
Joshua Johansen <joshuabrian.johansen@dk.experis.com>
2022-03-08 15:05:45 +0100

    Added Id to Address

--- src/main/java/com/example/backend/model/Address.java
+++ src/main/java/com/example/backend/model/Address.java
@@ -1,4 +1,15 @@
 package com.example.backend.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "addresses")
 public class Address {
+    @Id
+    @Column(name = "address_id")
+    private UUID addressId = UUID.randomUUID();
 }
