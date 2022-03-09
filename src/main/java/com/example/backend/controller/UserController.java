77e9a342414f48057c494e3de57ec124414eca14
Allan Carlsen <allantcarlsen@gmail.com>
2022-03-09 14:51:01 +0100

    Added functionality to GET /user

--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -1,19 +1,28 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.User;
+import com.example.backend.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/User")
+@RequestMapping("/user")
 @RestController
 @Tag(name = "User")
 public class UserController {
 
+    final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("")
+    @Operation(summary = "Get all users")
     public List<User> getAllUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @PostMapping("")
