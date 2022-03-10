bff0b0ba5c94fdb2e13217bdd79e8afc66a4ccf1
“Allan <allantcarlsen@gmail.com>
2022-03-10 10:37:10 +0100

    Added functionality to ProfileController.

--- src/main/java/com/example/backend/controller/ProfileController.java
+++ src/main/java/com/example/backend/controller/ProfileController.java
@@ -1,24 +1,46 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.Profile;
+import com.example.backend.repository.ProfileRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/profile")
 @RestController
 @Tag(name = "Profile")
 public class ProfileController {
 
+    final ProfileRepository profileRepository;
+
+    public ProfileController(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
     @GetMapping("")
+    @Operation(summary = "Get all Profiles")
     public List<Profile> getAllProfiles() {
-        return null;
+        return profileRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get profile by ID")
+    public Profile getProfileById(@PathVariable int id){
+        Optional<Profile> profileExist = profileRepository.findById(id);
+        return profileExist.orElse(null);
     }
 
     @PostMapping("")
-    public void createProfile(@RequestBody Profile profile) {
-
+    @Operation(summary = "Create profile - returns created profile if success, else null")
+    public Profile createProfile(@RequestBody Profile profile) {
+        Optional<Profile> profileExist = profileRepository.findByEmail(profile.getEmail);
+        if(profileExist.isEmpty()){
+            return profileRepository.save(profile);
+        }
+        return null;
     }
 
 }
