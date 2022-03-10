2f92806018365072fe54f304f5835dbabb3a9d51
“Allan <allantcarlsen@gmail.com>
2022-03-10 15:27:04 +0100

    Added dummy-data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,87 @@
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('lorem', 'Jasmin', 'Sorel', 'jsorel0@usgs.gov', false, false);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('in', 'Marnie', 'Crimpe', 'mcrimpe1@opera.com', true, true);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('tempus', 'Melantha', 'Burness', 'mburness2@creativecommons.org', true, false);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('cum', 'Ward', 'D''Errico', 'wderrico3@japanpost.jp', true, false);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('curabitur', 'Saidee', 'Butter', 'sbutter4@xing.com', true, true);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('consequat', 'Jeffy', 'Davey', 'jdavey5@clickbank.net', true, true);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('aenean', 'Madel', 'McAuliffe', 'mmcauliffe6@howstuffworks.com', false, false);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('sed', 'Kelcey', 'Brashaw', 'kbrashaw7@netscape.com', false, true);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('elit', 'Dyann', 'Keilty', 'dkeilty8@twitpic.com', false, true);
+insert into users (password, first_name, last_name, email, is_contributor, is_admin) values ('dui', 'Lula', 'Dunsire', 'ldunsire9@statcounter.com', false, false);
+
+insert into addresses (address_line1, postal_code, city, country) values ('418 Independence Alley', 1182, 'Huangma', 'China');
+insert into addresses (address_line1, postal_code, city, country) values ('76652 8th Center', 4982, 'Radzyń Chełmiński', 'Poland');
+insert into addresses (address_line1, postal_code, city, country) values ('6 Hazelcrest Parkway', 1063, 'Ribafria', 'Portugal');
+insert into addresses (address_line1, postal_code, city, country) values ('624 Maywood Crossing', 388, 'Hufang', 'China');
+insert into addresses (address_line1, postal_code, city, country) values ('30077 Gale Lane', 8369, 'Sambongmulyo', 'Indonesia');
+insert into addresses (address_line1, postal_code, city, country) values ('93752 Straubel Trail', 9567, 'Waśniów', 'Poland');
+insert into addresses (address_line1, postal_code, city, country) values ('78 Stuart Terrace', 5279, 'Titiakar', 'Indonesia');
+insert into addresses (address_line1, postal_code, city, country) values ('17 Declaration Terrace', 2900, 'Besuk Selatan', 'Indonesia');
+insert into addresses (address_line1, postal_code, city, country) values ('6403 Norway Maple Lane', 8717, 'Tangxi', 'China');
+insert into addresses (address_line1, postal_code, city, country) values ('373 Park Meadow Avenue', 4182, 'Miaoshi', 'China');
+
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('vel', 'erat curabitur gravida nisi at nibh in hac habitasse platea', 'iaculis', 'http://dummyimage.com/146x100.png/ff4444/ffffff', 'http://dummyimage.com/131x100.png/dddddd/000000');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('dolor', 'malesuada in imperdiet et commodo vulputate justo in blandit ultrices enim lorem ipsum dolor sit amet', 'amet', 'http://dummyimage.com/245x100.png/ff4444/ffffff', 'http://dummyimage.com/124x100.png/cc0000/ffffff');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('montes', 'sapien arcu sed augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse', 'gravida', 'http://dummyimage.com/221x100.png/ff4444/ffffff', 'http://dummyimage.com/176x100.png/dddddd/000000');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('sem', 'nulla sed vel enim sit amet nunc viverra dapibus nulla suscipit', 'donec', 'http://dummyimage.com/122x100.png/dddddd/000000', 'http://dummyimage.com/221x100.png/5fa2dd/ffffff');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('curae', 'vehicula condimentum curabitur in libero ut massa volutpat convallis morbi odio odio elementum eu', 'vestibulum', 'http://dummyimage.com/153x100.png/cc0000/ffffff', 'http://dummyimage.com/197x100.png/ff4444/ffffff');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('pede', 'sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae', 'convallis', 'http://dummyimage.com/168x100.png/5fa2dd/ffffff', 'http://dummyimage.com/150x100.png/dddddd/000000');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('condimentum', 'venenatis tristique fusce congue diam id ornare imperdiet sapien urna pretium nisl ut volutpat sapien arcu sed augue aliquam erat', 'aliquet', 'http://dummyimage.com/193x100.png/5fa2dd/ffffff', 'http://dummyimage.com/158x100.png/5fa2dd/ffffff');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('risus', 'eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet', 'morbi', 'http://dummyimage.com/103x100.png/dddddd/000000', 'http://dummyimage.com/151x100.png/cc0000/ffffff');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('curabitur', 'at turpis a pede posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel', 'fermentum', 'http://dummyimage.com/178x100.png/dddddd/000000', 'http://dummyimage.com/170x100.png/cc0000/ffffff');
+insert into exercises (name, description, target_muscle_group, image, video_link) values ('orci', 'ante vivamus tortor duis mattis egestas metus aenean fermentum donec', 'volutpat', 'http://dummyimage.com/227x100.png/cc0000/ffffff', 'http://dummyimage.com/243x100.png/5fa2dd/ffffff');
+
+insert into sets (exercise_repetitions) values (34);
+insert into sets (exercise_repetitions) values (45);
+insert into sets (exercise_repetitions) values (83);
+insert into sets (exercise_repetitions) values (41);
+insert into sets (exercise_repetitions) values (11);
+insert into sets (exercise_repetitions) values (95);
+insert into sets (exercise_repetitions) values (94);
+insert into sets (exercise_repetitions) values (60);
+insert into sets (exercise_repetitions) values (37);
+insert into sets (exercise_repetitions) values (47);
+
+insert into profiles (weight, height, medical_conditions, disabilities) values (65, 17, 'pede', 'in');
+insert into profiles (weight, height, medical_conditions, disabilities) values (13, 49, null, null);
+insert into profiles (weight, height, medical_conditions, disabilities) values (81, 5, 'in', 'elit');
+insert into profiles (weight, height, medical_conditions, disabilities) values (93, 92, null, null);
+insert into profiles (weight, height, medical_conditions, disabilities) values (57, 70, null, null);
+insert into profiles (weight, height, medical_conditions, disabilities) values (97, 64, null, null);
+insert into profiles (weight, height, medical_conditions, disabilities) values (33, 73, null, null);
+insert into profiles (weight, height, medical_conditions, disabilities) values (16, 82, 'congue', 'dui');
+insert into profiles (weight, height, medical_conditions, disabilities) values (42, 59, null, null);
+insert into profiles (weight, height, medical_conditions, disabilities) values (24, 78, null, null);
+
+insert into goals (end_date, achieved) values ('2021-06-30', false);
+insert into goals (end_date, achieved) values ('2021-10-16', true);
+insert into goals (end_date, achieved) values ('2021-09-06', true);
+insert into goals (end_date, achieved) values ('2021-12-08', true);
+insert into goals (end_date, achieved) values ('2021-04-10', true);
+insert into goals (end_date, achieved) values ('2021-12-21', false);
+insert into goals (end_date, achieved) values ('2021-07-20', true);
+insert into goals (end_date, achieved) values ('2021-05-04', true);
+insert into goals (end_date, achieved) values ('2021-05-29', false);
+insert into goals (end_date, achieved) values ('2021-03-14', false);
+
+insert into workouts (name, type, complete) values ('orci', 'quam', true);
+insert into workouts (name, type, complete) values ('ipsum', 'potenti', true);
+insert into workouts (name, type, complete) values ('orci', 'at', true);
+insert into workouts (name, type, complete) values ('eu', 'ante', false);
+insert into workouts (name, type, complete) values ('id', 'parturient', true);
+insert into workouts (name, type, complete) values ('libero', 'ipsum', false);
+insert into workouts (name, type, complete) values ('vestibulum', 'ac', false);
+insert into workouts (name, type, complete) values ('sit', 'tristique', true);
+insert into workouts (name, type, complete) values ('nec', 'accumsan', true);
+insert into workouts (name, type, complete) values ('dapibus', 'dui', false);
+
+insert into programs (name, category) values ('gravida', 'vel');
+insert into programs (name, category) values ('vel', 'at');
+insert into programs (name, category) values ('interdum', 'magna');
+insert into programs (name, category) values ('duis', 'ultrices');
+insert into programs (name, category) values ('montes', 'leo');
+insert into programs (name, category) values ('sit', 'at');
+insert into programs (name, category) values ('aliquam', 'tincidunt');
+insert into programs (name, category) values ('nulla', 'congue');
+insert into programs (name, category) values ('tristique', 'gravida');
+insert into programs (name, category) values ('magnis', 'id');
