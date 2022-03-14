b82b9e91eae8a52470605b6257f5e1593171a2d5
Allan Carlsen <allantcarlsen@gmail.com>
2022-03-14 12:42:01 +0100

    Added @CrossOrigin

--- src/main/java/com/example/backend/controller/GoalsController.java
+++ src/main/java/com/example/backend/controller/GoalsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @RequestMapping("/goals")
 @RestController
 @Tag(name = "Goals")
