518a9525656b80a26cfa7ad6953a069968cdebb8
“Allan <allantcarlsen@gmail.com>
2022-03-17 14:26:45 +0100

    Added swagger documentation. Added mapping to getProgramById Removed checkByName from creatProgram, since it should not matter.

--- src/main/java/com/example/backend/controller/ProgramController.java
+++ src/main/java/com/example/backend/controller/ProgramController.java
@@ -3,6 +3,7 @@ package com.example.backend.controller;
 import com.example.backend.model.Program;
 import com.example.backend.model.Workout;
 import com.example.backend.repository.ProgramRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,21 +22,21 @@ public class ProgramController {
     }
 
     @GetMapping("")
+    @Operation(summary = "Get all programs")
     public List<Program> getAllPrograms() {
         return programRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a program by id")
     public Program getProgramById(@PathVariable Integer id) {
         Optional<Program> program = programRepository.findById(id);
         return program.orElse(null);
     }
 
     @PostMapping("")
+    @Operation(summary = "Create a new program")
     public Program createProgram(@RequestBody Program program) {
-        Optional<Program> programExist = programRepository.findByName(program.getName());
-        if(programExist.isEmpty()) {
-            return programRepository.save(program);
-        }
-        return null;
+        return programRepository.save(program);
     }
 }
